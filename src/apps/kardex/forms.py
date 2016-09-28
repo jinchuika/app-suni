3eadcdb4cff1eb91c42b56319cdd04e0e3f5d33c
carloss <csanchezz@thinkandmake.net>
2016-09-28 14:48:05 -0600

    modificacion kardex v0.1

--- src/apps/kardex/forms.py
+++ src/apps/kardex/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from django.forms import ModelForm, ModelChoiceField, formset_factory, modelformset_factory
+from django.forms import ModelForm, ModelChoiceField, formset_factory, modelformset_factory, widgets
 from .models import *
 
 
@@ -12,17 +12,64 @@ class FormularioEquipo(ModelForm):
 		fields = ['nombre_equipo']
 
 
-class FormularioEntrada(forms.ModelForm):
-	fecha = forms.CharField(widget=forms.TextInput(attrs={'class':'datepicker'}))
+class FormularioEntrada(forms.ModelForm):	
 	class Meta:
 		model = Entrada
 		fields = '__all__'
+		widgets= {
+		'equipo':forms.Select(attrs={'class':' form-control'}),
+		'fecha':forms.TextInput(attrs={'class':'datepicker form-control', 'placeholder': 'Fecha'}),
+		'cantidad':forms.NumberInput(attrs={'class':' form-control','placeholder': 'Cantidad'}),
+		'precio': forms.NumberInput(attrs={'class':' form-control', 'placeholder' : 'Precio'}),
+		'factura': forms.NumberInput(attrs={'class':' form-control', 'placeholder' : 'Factura'}),
+			
+
+		}
+
+	def clean_cantidad(self):
+		cantidad = self.cleaned_data.get('cantidad')
+		if cantidad <= 0:
+			raise forms.ValidationError("Este campo tiene que ser mayor a 0")
+		return cantidad	
+
+	def clean_precio(self):
+		precio = self.cleaned_data.get('precio')
+		if precio is not None and precio <= 0:
+			raise forms.ValidationError("Este campo tiene que ser mayor a 0")
+		tipo= self.cleaned_data.get('tipo_entrada')
+		if tipo == "Compra" and precio <=0 :
+			raise forms.ValidationError("Este campo es requerido ")
+
+		return precio
+
+
+
+
+	
+
 
 class FormularioSalida(forms.ModelForm):
-	fecha = forms.CharField(widget=forms.TextInput(attrs={'class':'datepicker'}))
 	class Meta:
 		model = Salida
 		fields = '__all__'
+		widgets= {
+		'equipo':forms.Select(attrs={'class':' form-control'}),
+		'fecha':forms.TextInput(attrs={'class':'datepicker form-control', 'placeholder': 'Fecha'}),
+		'cantidad':forms.NumberInput(attrs={'class':' form-control','placeholder': 'Cantidad'}),
+		'no_entrada': forms.Select(attrs={'class':' form-control'}),
+		}
+
+	def clean_cantidad(self):
+		cantidad = self.cleaned_data.get('cantidad')
+		if cantidad <= 0:
+			raise forms.ValidationError("Este campo tiene que ser mayor a 0")
+
+		existente = self.cleaned_data.get('equipo').existencia
+		if cantidad > existente:
+			raise forms.ValidationError("El campo tiene que ser menor o igual a la existente (" + str(existente) + ")" )	
+		return cantidad	
+
+	
 
 class FormularioProveedor(forms.ModelForm):
 	class Meta:
