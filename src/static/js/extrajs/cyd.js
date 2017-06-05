9d5c01316f23a2209cde6c5bd0ea63f3b9ff6569
Luis Carlos Contreras <jinchuika@gmail.com>
2017-06-05 15:54:50 -0600

    X-editable. Vistas de grupos, sedes y creación de grupos

--- src/static/js/extrajs/cyd.js
+++ src/static/js/extrajs/cyd.js
@@ -23,3 +23,29 @@
 
 	}
 }( window.BuscadorSede = window.BuscadorSede || {}, jQuery ));
+
+(function( GrupoDetail, $, undefined ) {
+	GrupoDetail.init = function () {
+		$('.editable').on('shown', function(e, editable) {
+			$('.datepicker').datepicker({
+				format: 'yyyy-mm-dd',
+				autoclose: true,
+				language: 'es'
+			});
+		}).editable({
+			ajaxOptions: {
+				contentType: 'application/json',
+				dataType: 'json',
+				type: "PATCH",
+				beforeSend: function(xhr, settings) {
+					xhr.setRequestHeader("X-CSRFToken", $('input[name="csrfmiddlewaretoken"]').val());
+				}
+			},
+			params: function(params) { 
+				var obj = {};
+				obj[params['name']] = params['value'];
+				return JSON.stringify(obj);
+			}
+		});
+	}
+}( window.GrupoDetail = window.GrupoDetail || {}, jQuery ));
