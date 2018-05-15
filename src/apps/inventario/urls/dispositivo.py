09a03735c5ca2736eb684e35eb059bce9223d843
Edgar Patzan <epatzan@github.com>
2018-05-15 11:03:35 -0600

    uso de triage

--- src/apps/inventario/urls/dispositivo.py
+++ src/apps/inventario/urls/dispositivo.py
@@ -4,9 +4,13 @@ from apps.inventario import views as inventario_v
 urlpatterns = [
     # Listado de Teclados
     url(
-        r'^teclados/list/$',
-        inventario_v.SoftwareListView.as_view(),
-        name='teclado_detail'
+        r'^teclado/(?P<triage>[\w\d-]+)/edit/$',
+        inventario_v.TecladoUpdateView.as_view(),
+        name='teclado_update'
         ),
+    url(
+        r'^teclado/(?P<triage>[\w\d-]+)/$',
+        inventario_v.TecladoDetailView.as_view(),
+        name='teclado_detail')
 
 ]
--- src/apps/inventario/views/dispositivo.py
+++ src/apps/inventario/views/dispositivo.py
@@ -1,5 +1,5 @@
 from django.shortcuts import reverse
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
 from braces.views import (
     LoginRequiredMixin, PermissionRequiredMixin
 )
@@ -7,6 +7,18 @@ from apps.inventario import models as inv_m
 from apps.inventario import forms as inv_f
 
 
+class TecladoUpdateView(LoginRequiredMixin, UpdateView):
+    model = inv_m.Teclado
+    form_class = inv_f.TecladoForm
+    slug_field = "triage"
+    slug_url_kwarg = "triage"
+    query_pk_and_slug = True
+    template_name = 'inventario/Dispositivos/Teclados/teclado_form.html'
+
+
 class TecladoDetailView(LoginRequiredMixin, DetailView):
     model = inv_m.Teclado
-    template_name = 'inventario/Dispositivo/Teclados/teclado_detail.html'
+    template_name = 'inventario/Dispositivos/Teclados/teclado_detail.html'
+    slug_field = "triage"
+    slug_url_kwarg = "triage"
+    query_pk_and_slug = True
