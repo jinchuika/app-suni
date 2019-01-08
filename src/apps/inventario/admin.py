7a60da70a8671885eed449d6cc708bfb6d400e28
Arnoldo Ruano <arnoldo.ruano6@gmail.com>
2019-01-08 12:22:36 -0600

    Admin Crear QR

--- src/apps/inventario/admin.py
+++ src/apps/inventario/admin.py
@@ -1,4 +1,38 @@
 from django.contrib import admin
 from django.apps import apps
+from django.conf.urls import url
+from django.http import HttpResponseRedirect
+from .models import Dispositivo
+
 for model in apps.get_app_config('inventario').models.values():
-    admin.site.register(model)
+	if model.__name__ != "Dispositivo":
+		admin.site.register(model)
+
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+	change_list_template = "inventario/admin/dispositivo_changelist.html"
+
+	def get_urls(self):
+		urls = super().get_urls()
+		my_urls = [
+			url(
+		        r'^dispositivo/qr/$',
+		        self.admin_site.admin_view(self.set_qr),
+		        name='qr'
+		    ),
+		]
+		return my_urls + urls
+
+	def set_qr(self, request):
+		dispositivos = self.model.objects.all().filter(valido=True)
+		print(dispositivos)
+		if len(dispositivos) > 0:
+			for dispositivo in dispositivos:
+				print(dispositivo)
+			if not dispositivo.codigo_qr:
+				dispositivo.crear_qrcode()
+		else:
+			print("No hay")
+		return HttpResponseRedirect("../")
+
+
