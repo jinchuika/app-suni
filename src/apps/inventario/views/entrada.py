91c12fd9a69c61b7ea1c2d0f9bfc7bf0e6906bff
Edgar Patzan <epatzan@github.com>
2019-05-06 07:51:58 -0600

    permisos asignados para control de calidad sobre entradas

--- src/apps/inventario/views/entrada.py
+++ src/apps/inventario/views/entrada.py
@@ -36,7 +36,7 @@ class EntradaDetailView(LoginRequiredMixin, GroupRequiredMixin, DetailView):
     """
     model = inv_m.Entrada
     template_name = 'inventario/entrada/entrada_detail.html'
-    group_required = [u"inv_bodega", u"inv_tecnico", u"inv_admin"]
+    group_required = [u"inv_bodega", u"inv_tecnico", u"inv_admin", u"inv_cc"]
 
 
 class EntradaListView(LoginRequiredMixin, GroupRequiredMixin, FormView):
@@ -46,7 +46,7 @@ class EntradaListView(LoginRequiredMixin, GroupRequiredMixin, FormView):
     model = inv_m.Entrada
     template_name = 'inventario/entrada/entrada_list.html'
     form_class = inv_f.EntradaInformeForm
-    group_required = [u"inv_bodega", u"inv_tecnico", u"inv_admin"]
+    group_required = [u"inv_bodega", u"inv_tecnico", u"inv_admin", u"inv_cc"]
 
 
 class EntradaUpdateView(LoginRequiredMixin, GroupRequiredMixin, UpdateView):
@@ -55,7 +55,7 @@ class EntradaUpdateView(LoginRequiredMixin, GroupRequiredMixin, UpdateView):
     model = inv_m.Entrada
     form_class = inv_f.EntradaUpdateForm
     template_name = 'inventario/entrada/entrada_edit.html'
-    group_required = [u"inv_bodega", u"inv_tecnico", u"inv_admin"]
+    group_required = [u"inv_bodega", u"inv_tecnico", u"inv_admin", u"inv_cc"]
 
     def get_success_url(self):
         if self.object.en_creacion:
