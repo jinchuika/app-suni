4cade7687af1ea990183e37b6c786ab9b2153021
Arnoldo Ruano <arnoldo.ruano6@gmail.com>
2020-05-27 19:01:53 -0600

    Constancia de Maestros
    Constancia para Maestros

--- src/apps/certificado/views.py
+++ src/apps/certificado/views.py
@@ -5,6 +5,7 @@ import shutil
 import os
 import datetime
 import locale
+import sys
 from django.shortcuts import render
 from apps.certificado import forms as certificado_f
 from django.views.generic import TemplateView, FormView, DetailView, View
@@ -47,6 +48,7 @@ class ListadoMaestroView(TemplateView):
              params = {'dpi': self.request.GET['dpi']}
              try:
                 resp = requests.get(url=url, params= params)
+                print(resp)
              except:
                 print("en espera")
         else:
@@ -76,6 +78,7 @@ class ListadoMaestroView(TemplateView):
                # Recorrer Sedes del Maestro
                for sede in sedes:
                 asignaciones = []
+                cursos = []
                 suma_curso = 0
                 naat = False
                 grupo_naat = 1
@@ -117,7 +120,7 @@ class ListadoMaestroView(TemplateView):
                context['validacion'] = 1
 
         except:
-          print("error")
+          print("Oops!", sys.exc_info()[0], "occurred.")
           context['validacion'] = 0
         return context
 
@@ -255,7 +258,112 @@ class DiplomaPdfView(View):
          else:
             return HttpResponse("No tiene asignado este curso")
 
+class ConstanciaPdfView(View):
+   """ Vista  encargada de generar los diplomas que se entregaran a los participantes de los cursos, obteniendo el dpi
+    para mostrar el resultado de que diploma se va a imprimir se puede  descargar el pdf del archivo tambien
+   """
+   def get( self, request, *args, **kwargs):
+      # Obtener Parametros
+      id_asignacion = self.request.GET['asignacion']
 
+      # Inicializar Variables
+      curso_asignado = 0
+      curso_aprobado = 0
+      curso_naat = 0
+      url_perfil = str("https://suni.funsepa.org/")+ str(reverse_lazy('listado')) + str("?dpi=")+str(self.request.GET['dpi'])
+      url = settings.LEGACY_URL['certificado']
+      locale.setlocale(locale.LC_ALL, 'es_GT.utf8')
+
+      # Se hace la peticion al servidor de suni-capacitacion para mostrar los datos
+      if url is not '':
+             params = {'dpi': self.request.GET['dpi']}
+             try:
+                resp = requests.get(url=url, params= params)
+             except:
+                print("en espera")
+      else:
+             print("el dpi esta vacio")
+      data= resp.json()
+
+      # Se valida si se obtuvo información
+      if len(data) is 0:
+         return HttpResponse("El dpi ingresado no es valido")
+      else:
+         nota_curso = 0
+         nota_minima = 0
+         year = 0
+         url_constancia = ''
+         asignaciones = []
+
+         for  registro in data:
+            if int(registro['numero_asignacion']) == int(id_asignacion):
+              nota_curso = int(registro['nota'])
+              nota_minima = int(registro['nota_minima'])
+              year = datetime.datetime.strptime(registro['fecha_final'], '%Y-%m-%d').date().year
+              url_constancia = str(registro['constancia'])
+              asignaciones.append(registro)
+
+         if year != datetime.date.today().year:
+            return HttpResponse("Curso No Válido")
+
+         if len(asignaciones) > 0:
+            if nota_curso >= nota_minima:
+              #Creacion de codigos QR
+              qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=6,
+                border=1,)
+
+              qr.add_data(url_perfil)
+              qr.make(fit=True)
+              filename = 'perfil-{}.png'.format(self.request.GET['dpi'])
+              ruta_origin = str(os.getcwd()) +str("/")+ str(filename)
+              ruta_qr = str(settings.MEDIA_ROOT) + str("qr_perfil/")+str(filename)
+
+              #mover codigo QR a la carpeta correspondiente
+              if not (os.path.isfile(ruta_qr)):
+                img = qr.make_image()
+                img.save(filename)
+                shutil.move(ruta_origin,ruta_qr)
+
+              #creacion de tipo de hoja y asignar imagen para colocar de fondo
+              h=letter
+              ruta_diploma = str(settings.STATICFILES_DIRS[0] ) + str("/css/constancia/") + str(url_constancia)
+
+              #obtener tipo de fuente que se le aplicara al nombre del diploma
+              ruta_ttf = str(settings.STATICFILES_DIRS[0] ) + str("/css/diploma/EdwardianScriptITC.ttf")
+              registerFont(TTFont('Edwardian',ruta_ttf))
+              response = HttpResponse(content_type='application/pdf')
+              response['Content-Disposition'] = 'filename="ConstanciaFunsepaTB-{}.pdf"'.format(str(self.request.GET['dpi']))
+
+              #Creacion de canvas con el programa reportlab para colocar todos los elementos del diploma
+              buffer = BytesIO()
+              c = canvas.Canvas(buffer, pagesize=(landscape(letter)))
+              x = 400
+              y = 230
+              w = 103
+              h = 119
+
+              #creacion  de la imagen que se colocora de fondo
+              c.drawImage(ruta_diploma, 0,0,width=792,height=612,anchor='sw',anchorAtXY=True,showBoundary=False)
+              c.drawImage(ruta_qr,35,45,width=50,height=50,anchor='sw',anchorAtXY=True,showBoundary=False)
+              c.setFont("Edwardian",50,leading=None)
+              c.setFillColor((0,0,0))
+              c.drawCentredString(x+w*0.0,y+h*0.5, str(data[0]['nombre'])+" "+str(data[0]['apellido']))
+              c.setFont("Times-Bold",15,leading=None)
+              c.drawCentredString(x+w*0.0,60+h*0.5, "Guatemala, " + datetime.datetime.now().strftime("%d de %B del %Y"))
+              c.setTitle('Constancia Funsepa')
+              c.showPage()
+              c.save()
+              pdf = buffer.getvalue()
+              buffer.close()
+              response.write(pdf)
+              return response
+            else:
+              return HttpResponse("Tienes nota de reprobado")
+         else:
+            return HttpResponse("No tiene asignado este curso")
 
 
 
