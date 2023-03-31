545de0eeb344b374ef77d2370ca5d5df2e0003c7
Arnoldo Ruano <arnoldo.ruano6@gmail.com>
2023-03-30 20:22:42 -0600

    Update views.py

--- src/apps/conta/views.py
+++ src/apps/conta/views.py
@@ -48,18 +48,14 @@ def get_existencia(tipo_dispositivo, fecha, periodo):
         dispositivo__in=bajas).exclude(dispositivo__in=compras).values('dispositivo')
 
     # Obtener Precio Estandar Actual y Anterior
-    
-    if periodo.fecha_fin.year <2023:
+     try:
         precio = conta_m.PrecioEstandar.objects.filter(
             tipo_dispositivo=tipo_dispositivo,
             periodo=periodo,
-            inventario='dispositivo').first().precio       
-    else:      
-        precio_lote = conta_m.PrecioDispositivo.objects.filter(
-        dispositivo__in=utiles,
-        activo=True).aggregate(Sum('precio'))       
-        precio = precio_lote['precio__sum']        
-    
+            inventario='dispositivo').first().precio
+    except:
+        precio = 10
+   
 
     # Obtener Precio Total
     if periodo.fecha_fin.year <= 2018:
@@ -67,14 +63,9 @@ def get_existencia(tipo_dispositivo, fecha, periodo):
             dispositivo__in=utiles,
             periodo__in=periodos_anteriores).aggregate(Sum('precio'))
      else:    
-           if periodo.fecha_fin.year <2023:            
-            precio_tipo_dispositivo = conta_m.PrecioDispositivo.objects.filter(
+           precio_tipo_dispositivo = conta_m.PrecioDispositivo.objects.filter(
             dispositivo__in=utiles,
             periodo=periodo).aggregate(Sum('precio'))
-        else:           
-            precio_tipo_dispositivo = conta_m.PrecioDispositivo.objects.filter(
-            dispositivo__in=utiles,
-            activo=True).aggregate(Sum('precio'))  
             
     precio_tipo_compras = conta_m.PrecioDispositivo.objects.filter(
         dispositivo__in=compras,
@@ -83,18 +74,8 @@ def get_existencia(tipo_dispositivo, fecha, periodo):
     if precio_tipo_dispositivo['precio__sum'] is not None:
         precio_tipo_dispositivo = precio_tipo_dispositivo['precio__sum']
     else:
-        if precio is not None:
-            if periodo.fecha_fin.year < 2023:
-                precio_tipo_dispositivo = len(utiles) * precio                
-            else:
-                precio_tipo_dispositivo = precio    
-        else:
-            precio_tipo_dispositivo = 0
-
-    if precio_tipo_compras['precio__sum'] is not None:
-        precio_tipo_compras = precio_tipo_compras['precio__sum']
-    else:
-        precio_tipo_compras = 0
+       if precio is not None:
+            precio_tipo_dispositivo = len(utiles) * precio
 
     precio_total = precio_tipo_dispositivo + precio_tipo_compras
     existencia = len(utiles) + len(compras)
