990edb0205d94af07d254dced5949476f7fd0bb8
Arnoldo Ruano <arnoldo.ruano6@gmail.com>
2023-04-27 21:09:47 -0600

    Update views.py

--- src/apps/conta/views.py
+++ src/apps/conta/views.py
@@ -66,18 +66,7 @@ def get_existencia(tipo_dispositivo, fecha, periodo):
             dispositivo__in=utiles,
             periodo__in=periodos_anteriores).aggregate(Sum('precio'))        
     else:
-        if periodo.fecha_fin.year <2023:            
-            precio_tipo_dispositivo = conta_m.PrecioDispositivo.objects.filter(
-            dispositivo__in=utiles,
-            periodo=periodo).aggregate(Sum('precio'))
-        else:           
-            precio_tipo_dispositivo = conta_m.PrecioDispositivo.objects.filter(
-            dispositivo__in=utiles,
-            activo=True).aggregate(Sum('precio'))    
-    precio_tipo_compras = conta_m.PrecioDispositivo.objects.filter(
-        dispositivo__in=compras,
-        activo=True).aggregate(Sum('precio'))
-
+ 
     if precio_tipo_dispositivo['precio__sum'] is not None:       
         precio_tipo_dispositivo = precio_tipo_dispositivo['precio__sum']        
     else:
